f854b8e266e75e7d667d500e5b2104a37ed75520
Abrorxon-abi <ppvbuyppv@gmail.com>
2024-07-29 16:08:03 +0500

    installed tailwind

---
+++ notcoin/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-3xl text-red-600">Notcoin-clone</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
